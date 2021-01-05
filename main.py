f3a7b6ac73b67c03bf05ae333bf25c68222dcebd
yinchnag <yuz942628@outlook.com>
2021-01-05 10:10:34 +0800

    push text

---
+++ main.py
@@ -0,0 +1,13 @@
+# TODO: 学习PY
+def print_hi(name):
+
+    val = 2
+    if val == 1:
+        print(f'val: {val}')
+    elif val > 10:
+        print(f'Error val:{val}')
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
